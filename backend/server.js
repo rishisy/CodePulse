c2405a1bb55ba6f86c49dabbc8feca055efc2330
mohak1763 <mohak1763.be21@chitkara.edu.in>
2024-04-06 02:30:15 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,15 @@ import cors from "cors";
 
 dotenv.config();
 mongoose.connect(
-  "mongodb://0.0.0.0:27017/codepulse",
+  "mongodb+srv://mohak1763be21:6tzHqqcXT72uLiDO@cluster0.vd9ys2m.mongodb.net/codePulse",
   () => {
     console.log("db connected");
   },
   (e) => console.log(e)
 );
 
-const PORT = process.env.SERVER_PORT || 9000;
-const origin = process.env.CORS_ORIGIN || "http://localhost:3000";
+const PORT = 9000;
+const origin = "http://localhost:3000";
 
 const app = express();
 
@@ -27,6 +27,10 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(api);
+
+app.get("/", (req, res) => {
+  res.send("Server Running!");
+});
 
 app.listen(PORT, () => {
   console.log(`Your app is running in http://localhost:${PORT}`);
